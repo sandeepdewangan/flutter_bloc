160972dee9ebcd5dd710712dc35e738ddf56c226
customprojectindia <customprojectindia@gmail.com>
2024-04-15 00:00:53 +0530

    Cubit, Stream Controller youtube link added

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-# bloc_examples
+# Bloc Examples
+[Youtube](https://www.youtube.com/watch?v=Mn254cnduOY)
+[Source Code](https://github.com/vandadnp/youtube-course-bloc)
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Cubit
+Cubit is the simple case bloc, a stream component that uses functions to communicate interactions from widgets to the cubit and a stream that widgets consume for which state changes are emitted.
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,11 +24,71 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+const names = [
+  'Sandeep',
+  'Khushbu',
+  'Cherry',
+];
+
+class NamesCubit extends Cubit<String?> {
+  NamesCubit() : super(null);
+
+  void pickRandomName() {
+    int rand = Random().nextInt(2);
+    String name = names.elementAt(rand);
+    emit(name);
+  }
+}
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // initialize cubit
+  late final NamesCubit cubit;
+  @override
+  void initState() {
+    super.initState();
+    cubit = NamesCubit();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cubit.close();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: StreamBuilder<String?>(
+        stream: cubit.stream,
+        builder: ((context, snapshot) {
+          final button = TextButton(
+            onPressed: () => cubit.pickRandomName(),
+            child: const Text("Generate random names"),
+          );
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return button;
+            case ConnectionState.waiting:
+              return button;
+            case ConnectionState.active:
+              return Column(
+                children: [
+                  Text(snapshot.data ?? ''),
+                  button,
+                ],
+              );
+            case ConnectionState.done:
+              return const SizedBox();
+          }
+        }),
+      ),
+    );
   }
 }
